7270812c27a5b89962eb0ba4e61aacfbe8cfb5ff
islam-Attar <islamattar95@gmail.com>
2022-06-18 23:04:15 +0300

    Lab 37

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,6 @@ const Header = (props) => {
                             variant="h6"
                             noWrap
                             component="a"
-                            // href="/"
 
                             sx={{
                                 mr: 2,
